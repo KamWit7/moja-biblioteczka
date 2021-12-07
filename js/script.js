7b37908652c7049430e35340ec17ea4f2d9a77f7
KamWit7 <kamilw77@interia.pl>
2021-12-07 22:42:41 +0100

    :books:

---
+++ js/script.js
@@ -0,0 +1,46 @@
+const qs = (query) => document.querySelector(query)
+const create = (elem) => document.createElement(elem)
+
+const booksList = qs(".books")
+const form = qs(".form")
+
+const displayBooks = (books) => {
+  booksList.firstElementChild ? (booksList.innerHTML = "") : ""
+  books.forEach(({ title, author, genre, rating }) => {
+    let bookItem = create("li")
+    let p = create("p")
+
+    p.innerHTML = `Title: ${title}, </br> Author: ${author}, </br> Genre: ${genre}, Rating: ${rating}`
+    bookItem.append(p)
+    booksList.append(bookItem)
+  })
+}
+
+const getBooks = () => JSON.parse(localStorage.getItem("books"))
+
+const addCurentBooks = (e) => {
+  e.preventDefault()
+
+  const {
+    elements: { title, author, genre, rating },
+  } = e.currentTarget
+
+  let book = {
+    title: title.value,
+    author: author.value,
+    genre: genre.value,
+    rating: rating.value,
+  }
+
+  let books = getBooks() ? [...getBooks(), book] : [book]
+  localStorage.setItem("books", JSON.stringify(books))
+
+  displayBooks(books)
+  form.reset()
+}
+
+getBooks()
+  ? displayBooks(getBooks())
+  : alert("You don't have eny saved books 😥")
+
+form.addEventListener("submit", addCurentBooks)
